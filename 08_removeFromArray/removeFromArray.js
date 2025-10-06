5aa5dc7e50edd6127f97859fbc42e23e62b1f44b
HorrySheet11 <marinnathan11@gmail.com>
2025-10-06 18:14:24 +0800

    completed 1st and 2nd test in removeFromArray.spec
    completed the1st and 2nd requirements for removeFromArray.spec.js

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,num1, num2) {
+    let newArr = arr;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === num1 || arr[i] === num2){
+            newArr.splice(i, 1);
+            i--;
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line
