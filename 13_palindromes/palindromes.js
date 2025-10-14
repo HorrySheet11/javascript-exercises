09fb01531b144da2502fe28219d558222b7efd19
HorrySheet11 <marinnathan11@gmail.com>
2025-10-14 18:22:09 +0800

    completed palindromes exercise
    passed all tests in palindromes.spec.js

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let replaced = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    console.log(replaced);
+    return replaced === replaced.split('').reverse().join('');
 };
 
 // Do not edit below this line
