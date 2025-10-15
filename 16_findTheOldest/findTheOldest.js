2bdb8e78617440c31892ea01c4b882a63e08964c
HorrySheet11 <marinnathan11@gmail.com>
2025-10-15 11:04:04 +0800

    completed findTheOldest.js
    passed all tests in findTheOldest.spec.js

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, currentPerson) => {
+        function getAge(birth, death) {
+            if (!death) {
+                death = new Date().getFullYear();
+            }
+            return death - birth;
+        }
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge < currentAge ? currentPerson : oldest;
+    });
 };
 
 // Do not edit below this line
