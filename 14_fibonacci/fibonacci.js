549c7f3b0d81c82f6520ec97cc991b74191e4402
HorrySheet11 <marinnathan11@gmail.com>
2025-10-15 10:49:49 +0800

    completed fibonacci exercise
    passed all tests in fibonacci.spec.js

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let integer = num;
+    if(typeof num === 'string') {
+        integer = parseInt(num.replace(/[^0-9]/g, ''));
+    }
 
+    if (integer < 0) {
+        return "OOPS";
+    } else if (integer === 0) {
+        return 0;
+    } else if (integer === 1) {
+        return 1;
+    } else {
+        let a = 0;
+        let b = 1;
+        for (let i = 2; i <= integer; i++) {
+            let temp = a+b;
+            a = b;
+            b = temp;
+        }return b;
+    }    
 };
 
 // Do not edit below this line
